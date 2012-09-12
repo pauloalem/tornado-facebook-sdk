e6eeb1db3553cab2105087213dc6d47da92abddf
paulo alem <biggahed@gmail.com>
2012-09-12 01:22:18 -0300

    Created user with correct permissions, adjust test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
-test_user_id = "100004308311603"
+test_user_id = "100004430523129"
 test_app_key = "256248457829889|VdsmNePcAFQ6wQrMmU6Qmz2FVnw"
--- tests/test_graphapi.py
+++ tests/test_graphapi.py
@@ -2,8 +2,12 @@ from tornado import testing
 from facebook import facebook
 from tornado.ioloop import IOLoop
 from tests import test_user_id, test_app_key
+import datetime
 
 class GraphAPITestCase(testing.AsyncTestCase):
+
+    #TODO: create a test user for each run
+    # example: POST 256248457829889/accounts/test-users?installed=true&name=John Doe&permissions=publish_stream&method=POST&access_token=256248457829889|VdsmNePcAFQ6wQrMmU6Qmz2FVnw
 
     def get_new_ioloop(self):
         return IOLoop.instance()
@@ -13,14 +17,13 @@ class GraphAPITestCase(testing.AsyncTestCase):
         graph.get_object(test_user_id, callback=self.stop)
         response = self.wait()
         expected = {
-            u'first_name': u'Helen', 
-            u'last_name': u'Martinazzisen',
-            u'middle_name': u'Amdcjhcaafjc',
-            u'name': u'Helen Amdcjhcaafjc Martinazzisen',
+            u'first_name': u'John',
+            u'last_name': u'Doe',
+            u'name': u'John Doe',
             u'locale': u'pt_BR',
             u'gender': u'female',
-            u'link': u'http://www.facebook.com/people/Helen-Amdcjhcaafjc-Martinazzisen/100004308311603',
-            u'id': u'100004308311603'
+            u'link': u'http://www.facebook.com/people/John-Doe/100004430523129',
+            u'id': u'100004430523129'
         }
         self.assertDictEqual(expected, response)
 
@@ -33,5 +36,9 @@ class GraphAPITestCase(testing.AsyncTestCase):
     def test_put_object(self):
         #putting on a test user wall is not working
         graph = facebook.GraphAPI(test_app_key)
-        graph.put_object(test_user_id, "feed", self.stop, message="Another message in the Wall")
+        graph.put_object(test_user_id,
+                "feed",
+                self.stop,
+                message="Another message in the Wall {}".format(datetime.datetime.now()))
         response = self.wait()
+        self.assertIn('id', response)
