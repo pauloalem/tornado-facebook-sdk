8a66755dba080fc987b19c6b536997b2b60e3d9c
paulo alem <biggahed@gmail.com>
2012-09-17 15:39:25 -0300

    Use setuptools instead of distutils

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -11,8 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+__version__ = '0.1.0'
 
 from graphapi import GraphAPI, GraphAPIError
-
-__version__ = '0.1'
 __all__ = ('GraphAPI', 'GraphAPIError')
--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-from distutils.core import setup
-from facebook import __version__
-
-with open('README.rst') as file:
-    long_description = file.read()
+from setuptools import setup
 
 setup(
     name='tornado-facebook-sdk',
-    version=__version__,
+    version='0.1.0',
     license='OSI',
     description='A tornado based facebook graph api wrapper',
     author='Paulo Alem',
     author_email='biggahed@gmail.com',
     url='https://github.com/pauloalem/tornado-facebook-sdk',
     packages=['facebook'],
-    long_description=long_description
+    long_description=open('README.rst').read(),
+    install_requires=open("requirements.txt").read().split("\n"),
 )
